4dd16d75868413814b0f036fe87c09d056f696a3
Michael Mwebaze <michael_mwebaze@jsi.com>
2021-11-29 10:39:30 -0500

    first commit

---
+++ database/docker-build.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+
+set -e;
+
+# On failure: print usage and exit with 1
+function print_usage {
+  me=`basename "$0"`
+  echo "Usage: ./$me -v <dhis2 version> -d <data set>";
+  echo "Example: ./$me -v 2.20 -d sierra-leone";
+  exit 1;
+}
+
+function validate_parameters {
+  if [ -z "$DHIS2_VERSION" ] || [ -z "$DATA_SET" ]  ; then
+    print_usage;
+  fi
+}
+
+function patch_demo_db {
+  gunzip dhis2-db.sql.gz
+
+  # The demo database has the ability to run analytics disabled. This SQL enables the functionality.
+  echo "
+    INSERT INTO userrolemembers 
+    SELECT b.userroleid,a.userid from 
+    (SELECT userid from users where username = 'admin' ) a
+    CROSS JOIN (SELECT userroleid from userrole where name = 'System administrator (ALL)') b;
+  " >> dhis2-db.sql
+
+  gzip dhis2-db.sql
+}
+
+function fetch_data {
+  rm -rf dhis2-db.sql.gz
+
+  if [ $DATA_SET == "sierra-leone" ]; then
+    curl -L -o dhis2-db.sql.gz https://github.com/dhis2/dhis2-demo-db/blob/master/$DATA_SET/$DHIS2_VERSION/dhis2-db-$DATA_SET.sql.gz?raw=true
+  elif [ $DATA_SET == "trainingland" ]; then
+    curl -L -o dhis2-db.sql.gz https://github.com/dhis2/dhis2-demo-db/raw/master/trainingland/trainingland.sql.gz?raw=true
+  elif [ $DATA_SET == "world-adoption" ]; then
+    curl -L -o dhis2-db.sql.gz https://github.com/dhis2/dhis2-demo-db/raw/master/world-adoption/world-adoption.sql.gz?raw=true
+  fi
+  
+  patch_demo_db
+}
+
+while getopts "v:d:" OPTION
+do
+  case $OPTION in
+    v)  DHIS2_VERSION=$OPTARG;;
+    d)  DATA_SET=$OPTARG;;
+    \?) print_usage;;
+  esac
+done
+
+validate_parameters
+
+VERSION_TMP=${DHIS2_VERSION//[-._]/}
+
+fetch_data
+
+docker build -t pgracio/dhis2-db:$DHIS2_VERSION-$DATA_SET .
---
+++ docker-build.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+set -e;
+
+# On failure: print usage and exit with 1
+function print_usage {
+  me=`basename "$0"`
+  echo "Usage: ./$me -v <dhis2 version>";
+  echo "Example: ./$me -v 2.23";
+  exit 1;
+}
+
+function validate_parameters {
+  if [ -z "$DHIS2_VERSION" ] ; then
+    print_usage;
+  fi
+}
+
+while getopts "v:" OPTION
+do
+  case $OPTION in
+    v)  DHIS2_VERSION=$OPTARG;;
+    \?) print_usage;;
+  esac
+done
+
+validate_parameters
+
+current_dir=`pwd`
+releases_dir="releases/$DHIS2_VERSION"
+
+if [ ! -d "$releases_dir" ]; then
+    mkdir -p $releases_dir
+fi
+
+file_name=`date +dhis2-%Y%m%d.war`
+dt=`date '+%Y%m%d'`
+
+rm -f $current_dir/releases/dhis2.war
+
+wget -O "$current_dir/$releases_dir/$file_name" "https://s3-eu-west-1.amazonaws.com/releases.dhis2.org/$DHIS2_VERSION/dhis.war"
+
+cp -a "$current_dir/$releases_dir/$file_name" "$current_dir/releases/dhis2.war"
+
+# build new image using new dhis.war 
+image_id=$(docker build -q -t dhis2/dhis2-web:$DHIS2_VERSION-tomcat7-jre8-$dt .)
+
+echo "Image id: $image_id"
+docker tag $image_id dhis2/dhis2-web:$DHIS2_VERSION-tomcat7-jre8-latest
+
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker push dhis2/dhis2-web
